8d1987d5c04322cfa116b720a918ec3baf52becd
Felix Wittwer <dev@felixwittwer.de>
2017-05-31 21:11:58 +0200

    Add basic functionality for Strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+use std::string::String;
+
+pub trait Covfefe {
+    fn covfefe(self) -> String;
+}
+
+
+impl Covfefe for String {
+    fn covfefe(self) -> String {
+        let mut modified_string = String::from(self);
+        if modified_string.ends_with(" ") {
+            modified_string.push_str("covfefe");
+        } else {
+            modified_string.push_str(" covfefe");
+        }
+        modified_string
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
